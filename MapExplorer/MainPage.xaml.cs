a989bf08ca5a8b9f42d85e8e034c3ecd9eaf769f
awkwardusername <markjayson.fuentes@pup.edu.ph>
2013-11-30 21:26:55 +0800

    added Event class, and a bunch of things

--- MapExplorer/MainPage.xaml.cs
+++ MapExplorer/MainPage.xaml.cs
@@ -667,25 +667,22 @@ namespace MapExplorer
             MyMap.Layers.Add(mapLayer);
         }
 
-        private static T _download_serialized_json_data<T>(string url) where T : new()
-        {
-            using (var w = new WebClient())
-            {
+        private static T _download_serialized_json_data<T>(string url) where T : new() {
+            var w = new WebClient();
                 var json_data = string.Empty;
                 // attempt to download JSON data as a string
-                try
-                {
-                    json_data = w.DownloadString(url);
+                try {
+                    json_data = w.DownloadData(url);
                 }
                 catch (Exception) { }
                 // if string with JSON data is not empty, deserialize it to class and return its instance 
                 return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
-            }
+            
         } 
 
         private void GetEvents() {
             var url = "https://www.eventbrite.com/json/event_search?app_key=SK6HU3BS44LNJIBDYK&";
-            var json = 
+            var events = _download_serialized_json_data<Events>(url);
         }
 
         /// <summary>
