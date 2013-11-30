b6a8d1e2bd3690a16b83cb864e1667603d458538
awkwardusername <markjayson.fuentes@pup.edu.ph>
2013-11-30 21:55:23 +0800

    tried fixing workaround in DownloadString for Silverlight

--- MapExplorer/MainPage.xaml.cs
+++ MapExplorer/MainPage.xaml.cs
@@ -667,22 +667,34 @@ namespace MapExplorer
             MyMap.Layers.Add(mapLayer);
         }
 
-        private static T _download_serialized_json_data<T>(string url) where T : new() {
-            var w = new WebClient();
-                var json_data = string.Empty;
-                // attempt to download JSON data as a string
-                try {
-                    json_data = w.DownloadData(url);
-                }
-                catch (Exception) { }
-                // if string with JSON data is not empty, deserialize it to class and return its instance 
-                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+        private static void download_serialized_json_data(){
+            var url = "https://www.eventbrite.com/json/event_search?app_key=SK6HU3BS44LNJIBDYK&";
             
-        } 
+
+            var w = new WebClient();
+            string json_data = string.Empty;
+            // attempt to download JSON data as a string
+            
+            w.DownloadStringCompleted += new DownloadStringCompletedEventHandler(_download_serialized_json_data_Handler);
+
+            try {
+                w.DownloadStringAsync(new Uri(url));
+            } catch (Exception) {
+            }
+            
+            // if string with JSON data is not empty, deserialize it to class and return its instance 
+            
+        }
+
+        private static void _download_serialized_json_data_Handler(object sender, DownloadStringCompletedEventArgs e) {
+            if (!e.Cancelled && e.Error == null)
+            {
+                string json_data = (string)e.Result;
+            }
+        }
 
         private void GetEvents() {
-            var url = "https://www.eventbrite.com/json/event_search?app_key=SK6HU3BS44LNJIBDYK&";
-            var events = _download_serialized_json_data<Events>(url);
+            
         }
 
         /// <summary>
